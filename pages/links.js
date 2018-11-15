65026d23c9a7e6be30a484c1df90eab35bdd307a
WILL MEIER <willmeierart@gmail.com>
2018-11-14 18:55:04 -0700

    update links page

--- pages/links.js
+++ pages/links.js
@@ -2,11 +2,11 @@ import Head from '../components/Head'
 import Links from '../components/links'
 import { links as Ls } from '../lib/data'
 
-const ev = [ [ ...Ls ][0] ]
+const ev = [ [ ...Ls ][0], [ ...Ls ][1] ]
 
 export default function LinksPage () {
 	const shuffledArr = (() => {
-		let links = Ls.slice(1)
+		let links = Ls.slice(2)
 		let LINKS = [ ...links ]
 		let currentIndex = links.length
 		let temporaryValue
@@ -18,7 +18,7 @@ export default function LinksPage () {
 			links[currentIndex] = links[randomIndex]
 			links[randomIndex] = temporaryValue
 		}
-		return ev.concat(LINKS)
+		return ev.concat(links)
 	})()
 	return (
 		<div>
